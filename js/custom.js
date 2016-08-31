882f8407c00ca4bcff86bfa221d5047d2007f48b
Gleb Vlasyuk <gleb.vlasyuk@gmail.com>
2016-08-31 09:46:28 +0300

    tabs1

--- js/custom.js
+++ js/custom.js
@@ -51,7 +51,7 @@ $(function(){
 			infinite: true,
 			dots: false,
 			autoplay: true,
-			autoplaySpeed: 5000,
+			autoplaySpeed: 10000,
 			speed: 1000,
 			slidesToShow: 1,
 			slidesToScroll: 1,
